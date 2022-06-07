06ac1ca4d22b72dfec8f072d6345651684810366
Stoyan Stoyanov <stoyan.stoyanov@hedgehoglab.com>
2022-06-07 09:49:46 +0300

    initialised new swift package, with the one time password view code prototype

---
+++ Sources/HHLOneTimePasswordView/ShakeEffect.swift
@@ -0,0 +1,44 @@
+//
+//  ShakeEffect.swift
+//  OneTimePasswordView
+//
+//  Created by Stoyan Stoyanov on 06/06/22.
+//
+
+#if canImport(SwiftUI)
+
+import Foundation
+import SwiftUI
+
+// MARK: - Shake Effect
+
+/// A geometry effect that mimics what Apple password fields do when password is wrong.
+struct ShakeEffect: GeometryEffect {
+    
+    /// The horizontal distance at which the "shaked" view should be moved
+    let offset: CGFloat
+    
+    /// The speed at which the "shaked" view should be moved
+    let speed: CGFloat
+    
+    var animatableData: CGFloat
+    
+    /// Creates a new geometry shake effect, that mimics the shake the Apple password fields do
+    /// when the entered password is wrong.
+    ///
+    /// - Parameters:
+    ///   - shakes: The number of shakes that you want performed. Default value is 1.
+    ///   - translation: The horizontal distance at which the "shaked" view should be moved
+    ///   - speed: The speed at which the "shaked" view should be moved
+    init(shakes: Int = 1, translation: CGFloat = 6, speed: CGFloat = 4) {
+        animatableData = CGFloat(shakes)
+        self.offset = translation
+        self.speed = speed
+    }
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX: offset * sin(animatableData * speed * .pi), y: 0))
+    }
+}
+
+#endif
---
+++ Tests/HHLOneTimePasswordViewTests/HHLOneTimePasswordViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HHLOneTimePasswordView
+
+final class HHLOneTimePasswordViewTests: XCTestCase {
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(HHLOneTimePasswordView().text, "Hello, World!")
+//    }
+}
